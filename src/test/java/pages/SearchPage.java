f4d69c015b0e402ae1b108ede05da1e1b0595dac
glorystraw <glorystraw@users.noreply.github.com>
2019-01-23 08:30:59 +0300

    TestProject Selenium Webdriver with TestNG 8 actions

--- src/test/java/pages/SearchPage.java
+++ src/test/java/pages/SearchPage.java
@@ -7,20 +7,21 @@ import org.testng.Assert;
 public class SearchPage {
 
     private WebDriver driver;
+    private final By productItem = By.xpath("//section[@class='product-item ']");
+    private final By sectionWithMath = By.xpath("//section[@class='product-item '][contains(., 'Math')]");
+    private final By productItemWithCartButton = By.xpath("//section[@class='product-item ']//button[.='Add to cart']");
 
     public SearchPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public SearchPage checkSearchResultsNumber() {
-        driver.findElements(By.xpath("//section[@class='product-item ']")).size();
-        Assert.assertEquals(driver.findElements(By.xpath("//section[@class='product-item ']")).size(), 10);
+        Assert.assertEquals(driver.findElements(productItem).size(), 10);
         return this;
     }
 
     public SearchPage CheckSearchResultsTitles() {
-        driver.findElements(By.xpath("//section[@class='product-item '][contains(., 'Math')]")).size();
-        Assert.assertEquals(driver.findElements(By.xpath("//section[@class='product-item '][contains(., 'Math')]")).size(), 10);
+        Assert.assertEquals(driver.findElements(sectionWithMath).size(), 10);
 
 //        List<WebElement> elementList = driver.findElements(By.xpath("//aside[@id='ui-id-2']//h3[text()='Products']//following-sibling::div//div"));
 //            for ( WebElement element : elementList) {
@@ -30,9 +31,9 @@ public class SearchPage {
     }
 
     public SearchPage CheckSearchResultsAddToCartButtons() {
-        driver.findElements(By.xpath("//section[@class='product-item ']")).size();
-        driver.findElements(By.xpath("//section[@class='product-item ']//button[.='Add to cart']")).size();
-        Assert.assertTrue(driver.findElements(By.xpath("//section[@class='product-item ']//button[.='Add to cart']")).size() > driver.findElements(By.xpath("//section[@class='product-item ']")).size());
+        driver.findElements(productItem).size();
+        driver.findElements(productItemWithCartButton).size();
+        Assert.assertTrue(driver.findElements(productItemWithCartButton).size() > driver.findElements(productItem).size());
         return this;
     }
 
